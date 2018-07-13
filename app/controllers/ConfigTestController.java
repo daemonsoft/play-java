9d56a9859b57107a7f63a12fe4b6962527d24eb9
William Hincapié Medina - Ceiba Software <william.hincapie@ceiba.com.co>
2018-07-13 17:24:20 -0500

    Printed application name to Logger

--- app/controllers/ConfigTestController.java
+++ app/controllers/ConfigTestController.java
@@ -3,17 +3,18 @@ package controllers;
 import com.google.inject.Inject;
 import com.typesafe.config.Config;
 
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.*;
 
-public class ConfigTestController extends Controller{
+public class ConfigTestController extends Controller {
 
 	@Inject
 	Config config;
-	
-	public Result index() {		
+
+	public Result index() {
 		String applicationName = config.getString("application.name");
-		System.out.println("application.name = "+ applicationName);
-		return ok(config.getString("application.name"));
+		Logger.info("application.name = " + applicationName);
+		return ok(applicationName);
 	}
 }
