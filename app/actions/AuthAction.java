e89e16d4529ea76aa5e72528894e711ab8a37126
William Hincapié Medina <daemonsoft@gmail.com>
2018-07-16 14:38:44 -0500

    Added AuthAction to validate token exists

---
+++ app/actions/AuthAction.java
@@ -0,0 +1,22 @@
+package actions;
+
+import play.mvc.Action;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.mvc.Results;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+public class AuthAction extends Action.Simple {
+
+    private static final String TOKEN = "tkn";
+
+    @Override
+    public CompletionStage<Result> call(Http.Context ctx) {
+        if (ctx._requestHeader().headers().get(TOKEN).isEmpty()) {
+            return CompletableFuture.completedFuture(Results.forbidden());
+        }
+        return delegate.call(ctx);
+    }
+}
