6af2ebc298093f72d5ef05588a3624221a853671
William Hincapié Medina <daemonsoft@gmail.com>
2018-07-24 17:40:09 -0500

    Fix imports

--- app/models/IPersonRepository.java
+++ app/models/IPersonRepository.java
@@ -4,7 +4,6 @@ import com.google.inject.ImplementedBy;
 
 import java.util.List;
 import java.util.concurrent.CompletionStage;
-import java.util.stream.Stream;
 
 @ImplementedBy(PersonRepository.class)
 public interface IPersonRepository {
